088bb772ceace0a093557f2700e62269a801a1cf
Dylan GORGET <dylan.gorget@hotmail.fr>
2026-03-06 17:45:30 +0100

    feat: montage du router analytics sur /api/analytics

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 const toolsRoutes = require('./tools.routes');
+const analyticsRoutes = require('./analytics.routes');
 
 const router = Router();
 
 // Monte le router tools sur /api/tools
 router.use('/tools', toolsRoutes);
+
+// Monte le router analytics sur /api/analytics
+router.use('/analytics', analyticsRoutes);
 
 module.exports = router;
